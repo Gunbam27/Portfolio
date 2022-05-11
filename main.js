dab794f05739d8b5c2309e2d15ab2c0cc6c70d0e
Gunbam27 <mingmi_27@naver.com>
2022-05-11 23:31:16 +0900

    05.11

--- main.js
+++ main.js
@@ -63,31 +63,31 @@ arrowUp.addEventListener('click', () => {
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e) => {
-  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter == null) {
-    return;
-  }
-  //Remove selection from the previous item and select new one
-  const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
-  const target =
-    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-  target.classList.add('selected');
+// workBtnContainer.addEventListener('click', (e) => {
+//   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+//   if (filter == null) {
+//     return;
+//   }
+//   //Remove selection from the previous item and select new one
+//   const active = document.querySelector('.category__btn.selected');
+//   active.classList.remove('selected');
+//   const target =
+//     e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+//   target.classList.add('selected');
 
-  projectContainer.classList.add('anim-out');
-  setTimeout(() => {
-    projects.forEach((project) => {
-      console.log(project.dataset.type);
-      if (filter === '*' || filter === project.dataset.type) {
-        project.classList.remove('invisible');
-      } else {
-        project.classList.add('invisible');
-      }
-    });
-    projectContainer.classList.remove('anim-out');
-  }, 300);
-});
+//   projectContainer.classList.add('anim-out');
+//   setTimeout(() => {
+//     projects.forEach((project) => {
+//       console.log(project.dataset.type);
+//       if (filter === '*' || filter === project.dataset.type) {
+//         project.classList.remove('invisible');
+//       } else {
+//         project.classList.add('invisible');
+//       }
+//     });
+//     projectContainer.classList.remove('anim-out');
+//   }, 300);
+// });
 
 const sectionIds = [
   '#home',
@@ -116,34 +116,34 @@ function scrollIntoView(selector) {
   selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
-const observerOptions = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.3,
-};
-const observerCallback = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
-      const index = sectionIds.indexOf(`#${entry.target.id}`);
-      if (entry.boundingClientRect.y < 0) {
-        selectedNavIndex = index + 1;
-      } else {
-        selectedNavIndex = index - 1;
-      }
-    }
-  });
-};
-const observer = new IntersectionObserver(observerCallback, observerOptions);
-sections.forEach((section) => observer.observe(section));
+// const observerOptions = {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 0.3,
+// };
+// const observerCallback = (entries, observer) => {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+//       const index = sectionIds.indexOf(`#${entry.target.id}`);
+//       if (entry.boundingClientRect.y < 0) {
+//         selectedNavIndex = index + 1;
+//       } else {
+//         selectedNavIndex = index - 1;
+//       }
+//     }
+//   });
+// };
+// const observer = new IntersectionObserver(observerCallback, observerOptions);
+// sections.forEach((section) => observer.observe(section));
 
-window.addEventListener('wheel', () => {
-  if (window.scrollY === 0) {
-    selectedNavIndex = 0;
-  } else if (
-    Math.round(window.scrollY + window.innerHeight) >=
-    document.body.scrollHeight
-  ) {
-    selectedNavIndex = navItems.length - 1;
-  }
-  selectNavItem(navItems[selectedNavIndex]);
-});
+// window.addEventListener('wheel', () => {
+//   if (window.scrollY === 0) {
+//     selectedNavIndex = 0;
+//   } else if (
+//     Math.round(window.scrollY + window.innerHeight) >=
+//     document.body.scrollHeight
+//   ) {
+//     selectedNavIndex = navItems.length - 1;
+//   }
+//   selectNavItem(navItems[selectedNavIndex]);
+// });
