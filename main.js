5935f4585bdc225963444aebab46be0850958dc4
Gunbam27 <mingmi_27@naver.com>
2022-05-12 23:10:38 +0900

    05.12
    스크롤시 한 섹션씩 이동

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 'use strict';
+// 스크롤시 한 섹션씩 이동
+window.onload = function(){
+  const section = document.querySelectorAll('.section');
+  const sectionCount = section.length;
+  section.forEach(function(item, index){
+    item.addEventListener('mousewheel', function(e){
+      e.preventDefault();
+      let delta = 0;
+      
+      if (e.wheelDelta) {
+          delta = e.wheelDelta / 120;
+      }
+          
+      
+      let moveTop = window.scrollY;
+      let sectionSelector = section[index];
+      
+      // wheel down : 다음 섹션으로 이동
+      if (delta < 0){
+        if (sectionSelector !== sectionCount-1){
+          moveTop = window.pageYOffset + sectionSelector.nextElementSibling.getBoundingClientRect().top;
+        }
+      }
+      
+      // wheel up : 이전 섹션으로 이동
+      else{
+        if (sectionSelector !== 0){
+            moveTop = window.pageYOffset + sectionSelector.previousElementSibling.getBoundingClientRect().top;
+          }
+      }
+      window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+    });
+  });
+}
 
-// Make navbar transparet when it is on the top
+//스크롤시 네비게이션바 투명하게 만들기
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -11,41 +45,32 @@ document.addEventListener('scroll', () => {
   }
 });
 
-//Handle scrolling when tapping on the navbar menu
+//네비게이션 클릭시 해당 섹션으로 이동
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   console.log(event.target.dataset.link);
   const target = event.target;
   const link = target.dataset.link;
-  if (link == null) {
-    return;
-  }
   navbarMenu.classList.remove('open');
   scrollIntoView(link);
-  selectedNavItem(target);
 });
 
-//Navbar toglle button for small screen
+//네비게이션바 햄버거버튼 클릭시 메뉴보이는 토글 버튼
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
 });
 
-//Handle click on "Contact me" button on home
+//홈섹션의 See My Work 클릭시 Work섹션으로 이동
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
-  scrollIntoView('#contact');
+  scrollIntoView('#work');
 });
 
-//Make home slowly fade to tranparent as the window scrolls down
-const home = document.querySelector('.home__container');
-const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
-});
-
-//Show "arrow up" button when scrolling down
+//스크롤시 위로 버튼 보이기
 const arrowUp = document.querySelector('.arrow-up');
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   if (window.scrollY > homeHeight / 2) {
     arrowUp.classList.add('visible');
@@ -54,96 +79,12 @@ document.addEventListener('scroll', () => {
   }
 });
 
-//Handle click on the "arrow up" button
+//위로 버튼누르면 홈섹션으로 가기
 arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
-
-//Projects//
-const workBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-// workBtnContainer.addEventListener('click', (e) => {
-//   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-//   if (filter == null) {
-//     return;
-//   }
-//   //Remove selection from the previous item and select new one
-//   const active = document.querySelector('.category__btn.selected');
-//   active.classList.remove('selected');
-//   const target =
-//     e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-//   target.classList.add('selected');
-
-//   projectContainer.classList.add('anim-out');
-//   setTimeout(() => {
-//     projects.forEach((project) => {
-//       console.log(project.dataset.type);
-//       if (filter === '*' || filter === project.dataset.type) {
-//         project.classList.remove('invisible');
-//       } else {
-//         project.classList.add('invisible');
-//       }
-//     });
-//     projectContainer.classList.remove('anim-out');
-//   }, 300);
-// });
-
-const sectionIds = [
-  '#home',
-  '#about',
-  '#skills',
-  '#work',
-  '#testimonials',
-  '#contact',
-];
-const sections = sectionIds.map((id) => document.querySelector(id));
-const navItems = sectionIds.map((id) =>
-  document.querySelector(`[data-link="${id}"]`)
-);
-
-let selectedNavIndex = 0;
-let selectedNavItem = navItems[0];
-function selectNavItem(selected) {
-  selectedNavItem.classList.remove('active');
-  selectedNavItem = selected;
-  selectedNavItem.classList.add('active');
-}
-
+// 이동기능
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
-  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
-
-// const observerOptions = {
-//   root: null,
-//   rootMargin: '0px',
-//   threshold: 0.3,
-// };
-// const observerCallback = (entries, observer) => {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting && entry.intersectionRatio > 0) {
-//       const index = sectionIds.indexOf(`#${entry.target.id}`);
-//       if (entry.boundingClientRect.y < 0) {
-//         selectedNavIndex = index + 1;
-//       } else {
-//         selectedNavIndex = index - 1;
-//       }
-//     }
-//   });
-// };
-// const observer = new IntersectionObserver(observerCallback, observerOptions);
-// sections.forEach((section) => observer.observe(section));
-
-// window.addEventListener('wheel', () => {
-//   if (window.scrollY === 0) {
-//     selectedNavIndex = 0;
-//   } else if (
-//     Math.round(window.scrollY + window.innerHeight) >=
-//     document.body.scrollHeight
-//   ) {
-//     selectedNavIndex = navItems.length - 1;
-//   }
-//   selectNavItem(navItems[selectedNavIndex]);
-// });
